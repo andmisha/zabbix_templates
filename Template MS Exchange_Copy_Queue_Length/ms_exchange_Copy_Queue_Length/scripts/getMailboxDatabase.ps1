f30d0dddccc47700eb547d8b74db7c5a73c93169
Андреев Михаил Анатольевич <m.andreev@ACDAMATE.COM>
2023-06-07 12:44:41 +0300

    Add new templates and new catalog stucture

---
+++ Template MS Exchange_Copy_Queue_Length/ms_exchange_Copy_Queue_Length/scripts/getMailboxDatabase.ps1
@@ -0,0 +1,12 @@
+﻿#
+# Exchange (CopyQueueLenght) Template - version 1.1
+# for Zabbix 6.x.x
+#
+
+# Add MS Exchange 2016-2019 PowerShell module
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+# Get all Mailbox database list
+# For filter please add | where -Property Status -in "Mounted" after select construction
+$getMailboxDatabaseList= Get-MailboxDatabase | Get-MailboxDatabaseCopyStatus | select Name, Status | ConvertTo-Json
+# Write getMailboxDatabaseList on screen
+$getMailboxDatabaseList
---
+++ Template MS Exchange_Copy_Queue_Length/ms_exchange_Copy_Queue_Length/scripts/getMailboxDatabaseCopyQueueLength.ps1
@@ -0,0 +1,24 @@
+﻿#
+# Exchange (CopyQueueLenght) Template - version 1.1
+# for Zabbix 6.x.x
+#
+
+# Please enter input param mailDB name
+param($1)
+# Add MS Exchange 2016-2019 PowerShell module
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+# Check empty input param
+if($1 -ne $null)
+{
+    # Replace %%-prefix to \ (Zabbix discovery prototipe item key lifehack)
+    $replace = $1.replace('%%','\')
+    # Get CopyQueueLength
+    # For filter please add | {($_.Status -in "Mounted") -and before $_.Name condition
+    $getMailboxDatabaseCopyQueueLength = Get-MailboxDatabase | Get-MailboxDatabaseCopyStatus | select Name, CopyQueueLength | where -Property Name -in "$replace" | select CopyQueueLength
+    # Write CopyQueueLength on screen
+    $getMailboxDatabaseCopyQueueLength.CopyQueueLength
+}
+else
+{
+    write-host "No mailDB name found, please enter mailDB name!"
+}
